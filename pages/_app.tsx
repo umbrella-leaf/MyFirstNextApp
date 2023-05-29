5d98496bf264bd13ef382b0b8bb459fb20551cd3
Liu Zhongnuo <2313678365@qq.com>
2023-05-29 20:38:35 +0800

    取消对默认全局样式的引用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+// import '@/styles/globals.css';
 import type {AppContext, AppProps} from 'next/app';
 import App from "next/app";
 import {Layout, ILayoutProps} from "@/components/layout";
